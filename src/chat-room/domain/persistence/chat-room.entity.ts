8ab9deb69cdf2540e26605ea2a34a0d1cf93fc54
박지민 <pjmlove2008@gmail.com>
2025-03-28 15:30:26 +0900

    feat: (#1) chat-room 엔티티 연관관계 설정

--- src/chat-room/domain/persistence/chat-room.entity.ts
+++ src/chat-room/domain/persistence/chat-room.entity.ts
@@ -1,9 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { MessageEntity } from "src/message/domain/persistence/message.entity";
+import { UserEntity } from "src/user/domain/persistence/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('tbl_chat_room')
 export class ChatRoomEntity {
     @PrimaryGeneratedColumn('uuid', { name: 'chat_room_id'})
     id: string;
 
-    
+    @ManyToOne(() => UserEntity, (user) => user.startUserId)
+    startUser: UserEntity;
+
+    @ManyToOne(() => UserEntity, (user) => user.receiverUserId)
+    receiverUser: UserEntity;
+
+    @OneToMany(() => MessageEntity, (message) => message.chatroom)
+    @JoinColumn({name: 'message_id', referencedColumnName: 'id'})
+    message: MessageEntity[];
+
 }
